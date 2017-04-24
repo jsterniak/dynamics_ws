3268e1ddc6f6e41186e4527247a215268f726a0f
UbuntuROS <espadandy@gmail.com>
2017-04-23 19:35:05 -0700

    update video calibration

--- src/abb_irb120/irb120_cam_cali/include/CalibratedVideo.h
+++ src/abb_irb120/irb120_cam_cali/include/CalibratedVideo.h
@@ -5,16 +5,25 @@
 #include <ros/package.h>
 #include <sensor_msgs/Image.h>
 #include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
+#include <image_transport/image_transport.h>
+//#include <std_msgs/String.h>
 
 class CalibratedVideo{
 public:
-  CalibratedVideo(ros::NodeHandle n);
+	CalibratedVideo(ros::NodeHandle n);
 
   // Callback function.
-  void send_video(const sensor_msgs::ImageConstPtr& img);
-  ros::Subscriber img_sub;
-  ros::Publisher img_pub;
-  cv::Mat cameraMatrix, distCoeffs;
+	void send_video(const sensor_msgs::ImageConstPtr& img);
+//	void send_video(const std_msgs::String::ConstPtr& msg);
+	image_transport::Subscriber img_sub;
+//	ros::Subscriber img_sub;
+	image_transport::Publisher img_pub;
+//	ros::Publisher img_pub;	
+	cv::Mat cameraMatrix, distCoeffs;
+//	cv::VideoCapture capture(0);
+
 };
+
 
 #endif
--- src/abb_irb120/irb120_cam_cali/src/CalibratedVideo.cpp
+++ src/abb_irb120/irb120_cam_cali/src/CalibratedVideo.cpp
@@ -1,17 +1,19 @@
 #include "CalibratedVideo.h"
 
 #include <cv_bridge/cv_bridge.h>
-//#include <opencv2/core/core.hpp>
-
-
+//#include <sstream>
 using namespace cv;
 using namespace std;
 
 
 CalibratedVideo::CalibratedVideo(ros::NodeHandle n){
-	img_sub = n.subscribe("/usb_cam/image_raw", 10, &CalibratedVideo::send_video, this);
-	img_pub = n.advertise<sensor_msgs::Image>("/CalibratedVideo/image_fine",100);
+	image_transport::ImageTransport it(n);
+	img_sub = it.subscribe("/usb_cam/image_raw", 1, &CalibratedVideo::send_video, this);
+//	img_sub = n.subscribe("chatter", 1, CalibratedVideo::send_video, this);
+	img_pub = it.advertise("/CalibratedVideo/image_fine",1);
+//	img_pub = n.advertise<sensor_msgs::Image>("/CalibratedVideo/image_fine", 100);
 	
+
 	//read the calibrating parameters.
 	string path = ros::package::getPath("irb120_cam_cali");
 	const string inputSettingsFile = path + "/src/out_camera_data.xml";
@@ -25,18 +27,32 @@ CalibratedVideo::CalibratedVideo(ros::NodeHandle n){
 	fs["Camera_Matrix"] >> cameraMatrix;
 	fs["Distortion_Coefficients"] >> distCoeffs;
 	fs.release();
+//	for (int i=0;i<5;i++){
+//		ROS_INFO("%f ", distCoeffs.at<double>(i));
+//	}
+//	ROS_INFO("\n");
+
 }
 
+
 void CalibratedVideo::send_video(const sensor_msgs::ImageConstPtr& img){
-	Mat newFrame;
+//void CalibratedVideo::send_video(const std_msgs::String::ConstPtr& msg){
+	Mat rawFrame, newFrame;
 	cv_bridge::CvImagePtr inMsgPtr;
 	inMsgPtr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::BGR8);
 
-	undistort(inMsgPtr->image, newFrame, cameraMatrix, distCoeffs);
-	inMsgPtr->image = newFrame;
-	img_pub.publish(inMsgPtr->toImageMsg());
+	rawFrame = inMsgPtr->image;
+//	capture>>rawFrame;
 
-	ROS_INFO("sent frame width=%d height=%d", inMsgPtr->image.cols, inMsgPtr->image.rows);
+//	capture.read(rawFrame);
+	cv::undistort(rawFrame, newFrame, cameraMatrix, distCoeffs);
+	sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", newFrame).toImageMsg();
+	img_pub.publish(msg);
+
+//	for (int i=300; i<305;i++){
+//			ROS_INFO("%d %d %d %d %d", (int)rawFrame.at<uchar>(300,i),(int)rawFrame.at<uchar>(301,i),(int)rawFrame.at<uchar>(302,i),(int)rawFrame.at<uchar>(303,i),(int)rawFrame.at<uchar>(304,i));
+//	}
+//ROS_INFO("%d %d", rawFrame.rows, rawFrame.cols);
 }
 
 int main (int argc, char* argv[]){
@@ -47,3 +63,6 @@ int main (int argc, char* argv[]){
 
 	return 0;
 }
+
+
+
