9a15209e82254f106a0f67244b20815ce18a1707
jsterniak <antigrvty1@yahoo.com>
2017-05-01 14:30:42 -0400

    PCB registration integrated

--- src/abb_irb120/irb120_vision/include/irb120_vision.hpp
+++ src/abb_irb120/irb120_vision/include/irb120_vision.hpp
@@ -5,21 +5,33 @@
 #include <sensor_msgs/Image.h>
 #include <std_msgs/Int32.h>
 #include <image_transport/image_transport.h>
+#include <opencv2/opencv.hpp>
 
 class CSARAVision
 {
 public:
   CSARAVision();
+
+  typedef std::vector<cv::KeyPoint> fiducial_vec;
+
   void state_update_cb(const std_msgs::Int32ConstPtr& msg);
   void img_frame_cb(const sensor_msgs::ImageConstPtr& img);
 
 private:
-  int32_t robot_state_cur_;
+  void find_fiducials(const cv::Mat& im, fiducial_vec& fiducials);
+  void register_pcb(const fiducial_vec& fiducials, int32_t img_rows, int32_t img_cols);
+
+  template<typename T>
+  int32_t sgn(T val)
+  {
+    return (T(0) < val) - (val < T(0));
+  }
+
   ros::NodeHandle nh_;
   ros::Subscriber img_sub_;
-  ros::Subscriber state_sub_;
   image_transport::Publisher pcb_feature_pub_;
   image_transport::ImageTransport it_;
+  ros::Publisher pcb_pose_pub_;
 };
 
 #endif
